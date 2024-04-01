78e1ecd34af888fe869f40815838417fca1dd021
michael lima dos santos <134506984+michaeltads@users.noreply.github.com>
2024-04-01 11:39:26 -0300

    Add files via upload

---
+++ po/exe2/src/ImparesDescrescente.java
@@ -0,0 +1,10 @@
+public class ImparesDescrescente {
+    public static void main(String[] args) {
+        
+        for (int i = 1000; i > 0; i--) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
